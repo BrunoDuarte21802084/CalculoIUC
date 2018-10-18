a47ab8225feafd31d103831936144af36c60e83a
BrunoDuarte21802084 <44269381+BrunoDuarte21802084@users.noreply.github.com>
2018-10-18 17:12:13 +0100

    Initial commit

---
+++ app/src/main/java/pt/ulp/ei/calculoiuc/AtividadeCalculoIUC.java
@@ -0,0 +1,15 @@
+package pt.ulp.ei.calculoiuc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AtividadeCalculoIUC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividade_calculo_iuc);
+    }
+}
+
+
